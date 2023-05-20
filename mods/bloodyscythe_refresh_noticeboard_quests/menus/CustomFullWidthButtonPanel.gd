6206b54eb9a9448cf45470a64184b895c023ab83
Dave <16521341+davidmfritz@users.noreply.github.com>
2023-05-20 23:26:31 +0200

    Added confirmation dialog

--- mods/bloodyscythe_refresh_noticeboard_quests/menus/CustomFullWidthButtonPanel.gd
+++ mods/bloodyscythe_refresh_noticeboard_quests/menus/CustomFullWidthButtonPanel.gd
@@ -1,11 +1,15 @@
 extends Control
 
+const confirm_dialog_message: String = "This will abort all accepted noticeboard quests and generate new ones. Are you sure?";
+
 func _on_BackButton_pressed():
 	get_parent().cancel()
 
 func _on_CustomButton_pressed():
-	# TODO: confirmation prompt "do you really want to do this?"
-	
+	if yield (MenuHelper.confirm(confirm_dialog_message), "completed") and not SaveSystem.busy and not SceneManager.transitioning:
+		refresh_noticeboard_quests()
+
+func refresh_noticeboard_quests():
 	# remove all noticeboard quests from the quest log
 	for record in SaveState.noticeboard.quests:
 		var quest = SaveState.quests.get_quest(record.quest)
