421c06f30311a8c6fb122c55f18fd89e54f2cacb
Dave <16521341+davidmfritz@users.noreply.github.com>
2023-05-25 13:01:46 +0200

    Added enabled switch in settings

--- mods/bloodyscythe_refresh_noticeboard_quests/ModContentInfo.gd
+++ mods/bloodyscythe_refresh_noticeboard_quests/ModContentInfo.gd
@@ -1,11 +1,26 @@
 extends ContentInfo
 
+var setting_toggle_enabled: bool = true
+
+const MODUTILS: Dictionary = {
+	"settings": [
+		{
+			"property": "setting_toggle_enabled",
+			"type": "toggle",
+			"label": "Enabled",
+		},
+	],
+}
+
 func init_content() -> void:
 	assert(DLC.has_mod("cat_modutils", 0))
 	var modutils: ContentInfo = DLC.mods_by_id["cat_modutils"]
 	modutils.callbacks.connect_scene_ready("res://menus/noticeboard/NoticeboardMenu.tscn", self, "_on_NoticeboardMenu_ready")
 
 func _on_NoticeboardMenu_ready(scene: Control) -> void:
+	if !setting_toggle_enabled:
+		 return
+	
 	# remove default panel
 	scene.get_node("BackButtonPanel").queue_free()
 	
